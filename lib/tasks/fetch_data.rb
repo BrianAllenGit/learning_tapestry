59838911a29e4ec4636348ec91a6b055ecdc9191
BrianAllenGit <brianallen5193@gmail.com>
2020-07-12 19:55:14 -0700

    Update the fetch_data task to store response

--- lib/tasks/fetch_data.rb
+++ lib/tasks/fetch_data.rb
@@ -1,15 +1,18 @@
 require 'net/http'
 require 'httparty'
 require 'jwt'
+require 'json'
+require_relative "./../db"
 
 class FetchData
     def run(url = "https://staging.credentialengineregistry.org/envelopes")
+        resources = DB.connect[:resources]
         response = HTTParty.get(url, {format: :json})
         response.parsed_response.map { |envelope| 
             decoded_resource = JWT.decode envelope["resource"], OpenSSL::PKey::RSA.new(envelope["resource_public_key"]), false, { algorithm: 'RS256' }
-            puts decoded_resource
-        }
 
+            decoded_resource.map { |resource| resources.insert(:external_id => resource['@id'], :graph => resource['@graph'].to_json, :context => resource['@resource'].to_json) }
+        }
     end
 end
 

febe349471d5794f4848873db908e3b78a84323d
BrianAllenGit <brianallen5193@gmail.com>
2020-07-12 19:52:35 -0700

    Add base model for resource

---
+++ lib/models/resource.rb
@@ -0,0 +1,14 @@
+require_relative "./../db"
+
+class Resource   
+    def initialize()
+        unless DB.connect.table_exists?(:resources)
+            DB.connect.create_table :resources do
+                primary_key :id
+                column :external_id, String
+                column :graph, JSON
+                column :context, JSON
+            end
+        end
+    end 
+end
